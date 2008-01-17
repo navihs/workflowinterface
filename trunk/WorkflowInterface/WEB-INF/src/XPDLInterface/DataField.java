d9e9f977d392a94de2a623f6f1fc111b207f6bf9
laurent.millan <laurent.millan@61316732-c343-0410-9417-65da3792641f>
2008-01-17 13:40:18 +0000

    datafield

--- trunk/WorkflowInterface/WEB-INF/src/XPDLInterface/DataField.java
+++ trunk/WorkflowInterface/WEB-INF/src/XPDLInterface/DataField.java
@@ -5,6 +5,10 @@ public class DataField {
 	/* Attributs String */
 	private String initialValue;
 	private String length;
+	private String id;
+	private String name;
+	private String isArray;
+	private String dataType;
 	//private String dataType; //Faire un objet dataType ? 
 	
 	public DataField()
