16f1853c7574ad0d583cd8a83f916c9cd85019c3
laurent.millan <laurent.millan@61316732-c343-0410-9417-65da3792641f>
2008-01-17 13:45:32 +0000

    testJdom 1ere définition de méthodes WorkflowPackage

--- WorkflowInterface/WEB-INF/src/XPDLInterface/testJDOM.java
+++ WorkflowInterface/WEB-INF/src/XPDLInterface/testJDOM.java
@@ -30,23 +30,33 @@ public class testJDOM {
 
 	     racine = document.getRootElement();
 	}
-	
-	public String parsePackage()
+	public void parsePackage()
 	{
+		/*on lit le Header,
+		 * on créer un objet WorkflowPackage
+		 * on récupère l'élément <Package Id="dossier_accueil_individualise" Name="Dossier d'accueil individualisé" 
+		 * que l'on met dans WorkflowPackage.id et WorkflowPackage.name
+		 * on récupère l'élément <Created>2006-07-24 13:57:13</Created> que l'on met dans WorkflowPackage.date
+		 * on lance parseParticipant("Participants")
+		 * on récupère une liste de participant que l'on ajoute à WorkflowPackage.participants
+		 * on lance parseDatafields("DataFields")
+		 * on récupère une liste de dataFields que l'on ajoute à WorkflowPackage.dataFields
+		 * on lance parseWorkflowProcesses("WorkflowProcesses")
+		 * on récupère une liste de Worflow que l'on ajoute à WorkflowPackage.workflows
+		 * on lance parseExtendedAttributes("ExtendedAttributes")
+		 * on récupère une liste d'ExtendedAttribute que l'on ajoute à WorkflowPackage.extendedAttributes
+		 */
+		WorkflowPackage workflowPackage;
 		Element packageHeader = racine.getChild("PackageHeader");
-		Element created = packageHeader.getChild("Created");
+		workflowPackage = new WorkflowPackage(packageHeader.getAttribute("Id").getValue(),packageHeader.getAttribute("Name").getValue());
+		
 		DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd hh:mm:ss");
-		Date d=null;
+		Element created = packageHeader.getChild("Created");
 		try{
-			d = dateFormat.parse(created.getValue());
+			workflowPackage.setCreated(dateFormat.parse(created.getValue()));
 		}catch(Exception err){}
 		
-		return "err";
-	    /* Attributs String*/
-		/***
-		 * <!ELEMENT Created
-		 * <!ATTLIST Package id
-		 * <!ATTLIST Package name
-		 */
+		
 	}
+
 }
