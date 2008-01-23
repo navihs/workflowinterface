67744cdf7d9cf1c8cbae27bd241ae0ad9b18716b
laurent.millan <laurent.millan@61316732-c343-0410-9417-65da3792641f>
2008-01-23 13:21:33 +0000

    Afficheur

--- WorkflowInterface/WEB-INF/src/Vues/ModeleTexte.java
+++ WorkflowInterface/WEB-INF/src/Vues/ModeleTexte.java
@@ -18,8 +18,63 @@ public class ModeleTexte{
 
 	public static String activity(Activity a)
 	{
-		return "";
+		String s=" ";
+		s+="<table border=1 cellspacing=0 cellspacing=0>";
+		s+="<tr><td colspan=2 align=center>Activity</td></tr>";
+		s+="<tr>";
+		s+="<td>Id</td>";
+		s+="<td>"+a.getId()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Name</td>";
+		s+="<td>"+a.getName()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Descripion</td>";
+		s+="<td>"+a.getDescription()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>ExtendedAttributes ("+a.getExtendedAttributes().size()+")</td>";
+		s+="<td>"+ModeleTexte.listeExtendedAttributes(a.getExtendedAttributes())+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Implementation</td>";
+		s+="<td>"+a.getImplementation()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Limit</td>";
+		s+="<td>"+a.getLimit()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Performer</td>";
+		s+="<td><a href='Afficheur?action=doGetParticipant&id="+a.getPerformer().getId()+"'"+a.getPerformer().getName()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Route</td>";
+		s+="<td>"+a.getRoute()+"</td>";
+		s+="</tr>";
+		if(a.isSubflow())
+		{
+			s+="<tr>";
+			s+="<td>Subflow</td>";
+			s+="<td><a href='Afficheur.action=doGetWorkflow&id="+a.getSubflow().getId()+"'"+a.getSubflow().getName()+"</td>";
+			s+="</tr>";
+		}
+		s+="<tr>";
+		s+="<td>Transition Restriction Join</td>";
+		s+="<td>"+a.getTransitionRestrictionJoin()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Transition Restriction Split</td>";
+		s+="<td>"+a.getTransitionRestrictionSplit()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Transitions</td>";
+		s+="<td>Pas implemente</td>";
+		s+="</table>";
+		return s;
 	}
+	
 	
 	public static String participant(Participant p)
 	{
@@ -41,6 +96,116 @@ public class ModeleTexte{
 		s+="<tr>";
 		s+="<td>Description</td>";
 		s+="<td>"+p.getDescription()+"</td>";
+		s+="</tr>";
+		s+="</table>";
+		return s;
+	}
+	
+	public static String extendedAttribute(ExtendedAttribute ea)
+	{
+		String s=" ";
+		s+="<table border=1 cellspacing=0 cellspacing=0>";
+		s+="<tr><td colspan=2 align=center>ExtendedAttribute</td></tr>";
+		s+="<tr>";
+		s+="<td>Name</td>";
+		s+="<td>"+ea.getName()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Value</td>";
+		s+="<td>"+ea.getValue()+"</td>";
+		s+="</tr>";
+		s+="</table>";
+		return s;
+	}
+	
+	public static String formalParameter(FormalParameter fp)
+	{
+		String s=" ";
+		s+="<table border=1 cellspacing=0 cellspacing=0>";
+		s+="<tr><td colspan=2 align=center>ExtendedAttribute</td></tr>";
+		s+="<tr>";
+		s+="<td>Id</td>";
+		s+="<td>"+fp.getId()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>DataType</td>";
+		s+="<td>"+fp.getDataType()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<tr>";
+		s+="<td>Description</td>";
+		s+="<td>"+fp.getDescription()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Mode</td>";
+		s+="<td>"+fp.getMode()+"</td>";
+		s+="</tr>";
+		s+="</table>";
+		return s;
+	}
+	
+	public static String transition(Transition t)
+	{
+		String s=" ";
+		s+="<table border=1 cellspacing=0 cellspacing=0>";
+		s+="<tr><td colspan=2 align=center>ExtendedAttribute</td></tr>";
+		s+="<tr>";
+		s+="<td>Id</td>";
+		s+="<td>"+t.getId()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Condition</td>";
+		s+="<td>"+t.getCondition()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<tr>";
+		s+="<td>Condition type</td>";
+		s+="<td>"+t.getConditionType()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Extended Attributes</td>";
+		s+="<td>"+ModeleTexte.listeExtendedAttributes(t.getExtendedAttributes())+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>From</td>";
+		s+="<td><a href='Afficheur?action=doGetActivity&id="+t.getFrom().getId()+"'>"+t.getFrom().getName()+"</a></td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>To</td>";
+		s+="<td><a href='Afficheur?action=doGetActivity&id="+t.getTo().getId()+"'>"+t.getTo().getName()+"</a></td>";
+		s+="</tr>";
+		s+="</table>";
+		return s;
+	}
+	
+	public static String dataField(DataField df)
+	{
+		String s=" ";
+		s+="<table border=1 cellspacing=0 cellspacing=0>";
+		s+="<tr><td colspan=2 align=center>DataField</td></tr>";
+		s+="<tr>";
+		s+="<td>Id</td>";
+		s+="<td>"+df.getId()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Name</td>";
+		s+="<td>"+df.getName()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>DataType</td>";
+		s+="<td>"+df.getDataType()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>isArray</td>";
+		s+="<td>"+df.getIsArray()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Initial Value</td>";
+		s+="<td>"+df.getInitialValue()+"</td>";
+		s+="</tr>";
+		s+="<tr>";
+		s+="<td>Length</td>";
+		s+="<td>"+df.getLength()+"</td>";
 		s+="</tr>";
 		s+="</table>";
 		return s;
@@ -88,6 +253,7 @@ public class ModeleTexte{
 		String s=" ";
 		
 		Iterator<DataField> it = dataFields.iterator();
+		if(dataFields.size()==0) return "&nbsp";
 		while(it.hasNext())
 		{
 			DataField df = it.next();
@@ -101,6 +267,7 @@ public class ModeleTexte{
 		String s=" ";
 		
 		Iterator<ExtendedAttribute> it = extendedAttributes.iterator();
+		if(extendedAttributes.size()==0) return "&nbsp";
 		while(it.hasNext())
 		{
 			ExtendedAttribute ea = it.next();
@@ -114,6 +281,7 @@ public class ModeleTexte{
 		String s=" ";
 		
 		Iterator<Workflow> it = workflows.iterator();
+		if(workflows.size()==0) return "&nbsp";
 		while(it.hasNext())
 		{
 			Workflow wf = it.next();
